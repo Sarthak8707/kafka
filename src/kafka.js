b1c0802f6ecb1644e91bf9184aa8c3daa9863b20
sarthak <sarthakssingh49@gmail.com>
2026-05-02 00:07:12 +0530

    Kafka setup without secrets

---
+++ src/kafka.js
@@ -0,0 +1,43 @@
+import { Kafka } from "kafkajs";
+import fs from "fs";
+import "dotenv/config";
+
+export const kafka = new Kafka({
+  clientId: "my-app",
+
+  brokers: [
+    "kafka-25c4bf9c-sarthakssingh49-391d.j.aivencloud.com:28750"
+  ],
+
+  ssl: {
+    ca: [fs.readFileSync("./src/ca.pem", "utf-8")],
+  },
+
+  sasl: {
+    mechanism: "plain",
+    username: process.env.KAFKA_USERNAME ,
+    password: process.env.KAFKA_PASSWORD,
+  },
+});
+
+const producer = kafka.producer();
+
+async function run() {
+  try{
+    await producer.connect();
+
+  await producer.send({
+    topic: "test-topic",
+    messages: [
+      { value: "Hello from Kafka latest!!!" }
+    ],
+  });
+
+  console.log("Sent!");
+  }
+  catch(e){
+    console.log("Failed:", e);
+  }
+}
+
+run();
